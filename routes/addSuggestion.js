4002ad9adaad08a14291359fa754a47f5a682843
raghavddps2 <raghav.ddps2@gmail.com>
2019-04-02 18:33:52 +0530

    adding stuff

--- routes/addSuggestion.js
+++ routes/addSuggestion.js
@@ -8,7 +8,7 @@ router.get('/add-suggesstion',(req,res,next)=>{
     res.render('addSuggesstion');
 });
 router.post('/sugAdd',(req,res,next)=>{
-    suggestion.push({title: req.body.title, tag: req.body.tag,department:req.body.service,message:req.body.service});
+    suggestion.push({title: req.body.title, tag: req.body.tag,department:req.body.service,message:req.body.message});
     res.render('sugAdd');
 });
 
