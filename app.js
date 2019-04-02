fc5dcd1f86035e81f45c84bf15e2a4a6e6974c38
raghavddps2 <raghav.ddps2@gmail.com>
2019-04-02 18:24:47 +0530

    adding

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 
-
+const PORT = process.env.port|| 8080;
 app.set('view engine','pug');
 app.set('views','views');
 const addSuggRoute = require('./routes/addSuggestion');
@@ -27,5 +27,5 @@ app.get('/',(req,res,next)=>{
 app.use((req,res,next)=>{
     res.status(404).render('404');
 });
-const PORT = process.env.port||'8080';
+
 app.listen(PORT);
